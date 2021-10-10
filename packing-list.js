3301cca03275c1f9b32c3c7a8ce1a62c23173e11
SChopta <stefaniechopta@gmail.com>
2021-10-10 17:07:41 +0000

    packing-list.js

--- packing-list.js
+++ packing-list.js
@@ -1,6 +1,16 @@
-const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
-console.log("Kitchen stuff to pack:");
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];*/
+
+//1st version using a for loop
+/*console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
-}
+}*/
+
+
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}//2nd version using while loop
