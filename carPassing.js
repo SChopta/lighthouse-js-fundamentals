9692e6432a70b7d4a841dd2f01344a928aaf52d4
SChopta <stefaniechopta@gmail.com>
2021-10-11 22:52:58 +0000

    driving major daisy assignment

--- carPassing.js
+++ carPassing.js
@@ -1,28 +1,8 @@
-const cars = [
-  {
-    time: 1568329654807,
-    speed: 40,
-  },
-  {
-    time: 1568329821632,
-    speed: 42,
-  },
-  {
-    time: 1568331115463,
-    speed: 35
-  }
-]
-
-const carPassing = function (cars, speed) {
-  let newCar = {
+const carPassing = function(cars, speed) {
+  const newReading = {
     time: Date.now(),
-    speed: speed
+    speed
   };
-  cars.push(newCar); 
-  return cars; 
+  cars.push(newReading);
+  return cars;
 }
-
-console.log(carPassing(cars, 87)); 
-console.log(carPassing(cars, 34)); 
-console.log(carPassing(cars, 7)); 
-console.log(carPassing(cars, 47)); 
