45542c8888e3b8135ab0a8eb98abf31990594d54
SChopta <stefaniechopta@gmail.com>
2021-10-11 21:01:20 +0000

    Used conditionals to sort the array

--- voting-station.js
+++ voting-station.js
@@ -1,16 +1,23 @@
+const _stations = [
+  ['Big Bear Donair', 10, 'restaurant'],
+  ['Bright Lights Elementary', 50, 'school'],
+  ['Moose Mountain Community Centre', 45, 'community centre']
+];
+
+const output = ['Bright Lights Elementary', 'Moose Mountain Community Centre'];
+
 const chooseStations = (stations) => {
-  const goodStations = []
-  
-  for (const station of stations) {
-    
-    const  capacity = station[1]
-    
-    if (capacity >= 20) {
-      const type = station[2]
-      if (type === "community centre" || type === "school") {
-      goodStations.push(station[0])
-    }
-    }
+  if(stations && stations.length) {
+    // get the filtered stations 
+    return stations
+      .filter( station => {
+        const [ stationName, capacity, type ] = station; // array destructuring
+        if (capacity >= 20 && (type === 'school' || type === 'community centre')) {
+          return station;
+        } 
+      })
+      .map( s => s[0]);
   }
-  return goodStations
-}
+};
+  
+console.log(chooseStations(_stations));
