cf8f35ad4e1171589a70e928b85ade3700e28f95
SChopta <stefaniechopta@gmail.com>
2021-10-11 20:51:27 +0000

    Used conditionals to check criteria for numbers

--- loopylighthouse.js
+++ loopylighthouse.js
@@ -1,5 +1,6 @@
-for(let i=100;i<=200;i++)
-{
+let start = 100;
+let end = 200;
+for (let i = start; i < end + 1; i++) {
   if ((i % 3 === 0) && (i % 4 === 0)) {
     console.log("LoopyLighthouse");
   } else if (i % 3 === 0) {
@@ -8,5 +9,5 @@ for(let i=100;i<=200;i++)
     console.log("Lighthouse");
   } else {
     console.log(i);
-}
+  }
 }
